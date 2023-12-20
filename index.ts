a68f7445818003fe7baecdd8ef62721f93d55f11
Markus Savotškin <markus.savotskin@voco.ee>
2023-12-20 15:06:13 +0200

    Created models for online store.

---
+++ index.ts
@@ -0,0 +1,34 @@
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+
+mongoose.connect(
+  "db link"
+);
+const database = mongoose.connection;
+
+database.on("error", (error) => {
+  console.log(error);
+});
+
+database.once("connected", () => {
+  console.log("Database Connected");
+});
+
+const app: Express = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:3006"],
+  })
+);
+app.use(bodyParser.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(3000, () => {
+  console.log(`[server]: Server is running at http://localhost:3000`);
+});
